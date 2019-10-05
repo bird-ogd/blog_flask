c25b97f6362102a992a273b517b11940090f4198
bird-ogd <obird87@gmail.com>
2019-10-05 15:40:13 +0100

    Initial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+# Use .bash_profile file to create environment variables
+# import os
+
+class Config:
+	SECRET_KEY = 
+	SQLALCHEMY_DATABASE_URI =
+	MAIL_SERVER = 
+	MAIL_PORT = 
+	MAIL_USE_TLS = 
+	MAIL_USERNAME  = 
+	MAIL_PASSWORD = 
